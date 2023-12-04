40ab4d632b923633f2eb4ae6a438acc353fab8d8
Mikhail Nikorkin <nikorkin1990@mail.ru>
2023-12-04 21:17:20 +0300

    task7re

--- lesson-01/07.js
+++ lesson-01/07.js
@@ -1,4 +1,4 @@
-​let iAm = 'я';
+let iAm = "я";
 let krasavchik = "красавчик!";
-
-console.log(iAm.toUpperCase + " " + krasavchik)
+let result = iAm.toUpperCase() + " " + krasavchik;
+console.log(result);
