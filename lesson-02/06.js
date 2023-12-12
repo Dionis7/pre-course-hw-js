dc079ff2c196bdda23d4b01cec6d3fed0404166a
Mikhail Nikorkin <nikorkin1990@mail.ru>
2023-12-12 23:02:45 +0300

    weektwo2task6re

---
+++ lesson-02/06.js
@@ -0,0 +1,15 @@
+// Давайте-ка нашего парня, чтобы он не зазнавался переселим в ‘city’ 'Bobryisk' , а потом опять же убедимся в консоли, что мы все правильно скопировали. (Т.е. необходимо создать глубокую копию объекта passportWithAddress, а затем 'LA' заменить на 'Bobryisk' в city).
+
+let passportWithAddress = {
+  name: "Petr",
+  surname: "Petrov",
+  address: {
+    country: "USA",
+    city: "LA",
+  },
+};
+
+let cat = structuredClone(passportWithAddress);
+cat.address.city = "Bobryisk";
+console.log(passportWithAddress.address.city);
+console.log(cat.address.city);
